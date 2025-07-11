a363ceb3b22807b0a497cce53b62ab2e599fcd14
softdeveloper23 <brannongarrett77@gmail.com>
2025-07-11 16:06:29 +0200

    Added code for user input

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
-screen.exitonclick()
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+print(answer_state)
+
+turtle.mainloop()
+
